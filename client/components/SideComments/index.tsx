a52f741e4fb2e91b62702fccbf25965922b27ab4
danilbek <deni200040@gmail.com>
2021-12-27 05:52:57 +0300

    Dобавил возможность скрыть или показать компонент SideComments

--- client/components/SideComments/index.tsx
+++ client/components/SideComments/index.tsx
@@ -5,8 +5,19 @@ import styles from './SideComments.module.scss';
 import { CommentItem } from './CommentItem';
 import clsx from 'clsx';
 
-export const SideComments = () => (
-  <div className={clsx(styles.root)}>
-    {data.comments.popular.map((obj) => <CommentItem key={obj.id} {...obj} />)}
-  </div>
-);
+export const SideComments = () => {
+  const [visible, setVisible] = React.useState(true);
+
+  const toggleVisible = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className={clsx(styles.root, !visible && styles.rotated)}>
+      <h3 onClick={toggleVisible}>
+        Комментарии <ArrowRightIcon />
+      </h3>
+      {visible && data.comments.popular.map((obj) => <CommentItem key={obj.id} {...obj} />)}
+    </div>
+  );
+};
