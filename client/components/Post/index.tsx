00bdf2ce71c3775351badf121863b851fe9ea072
danilbek <deni200040@gmail.com>
2022-01-09 08:26:17 +0300

    Добавил иконки в компонент Post

--- client/components/Post/index.tsx
+++ client/components/Post/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Paper, Typography } from '@mui/material';
+import { IconButton, Paper, Typography } from '@mui/material';
 import styles from './Post.module.scss';
 import Image from 'next/image';
+import {
+  ModeCommentOutlined as CommentsIcon,
+  RepeatOutlined as RepostIcon,
+  BookmarkBorderOutlined as FavoriteIcon,
+  FileUploadOutlined as ShareIcon,
+  KeyboardArrowUp as ArrowUp,
+  KeyboardArrowDown as ArrowDown
+} from '@mui/icons-material';
 
 export const Post: React.FC = () => {
   return (
@@ -19,6 +27,35 @@ export const Post: React.FC = () => {
         height={400}
         alt=""
       />
+      <div className={styles.iconsContainer}>
+        <ul>
+          <li><IconButton>
+            <CommentsIcon />
+          </IconButton></li>
+          <li><IconButton>
+            <FavoriteIcon />
+          </IconButton></li>
+          <li><IconButton>
+            <ShareIcon />
+          </IconButton></li>
+          <li><IconButton>
+            <RepostIcon />
+          </IconButton></li>
+        </ul>
+        <ul>
+          <li>
+            <IconButton>
+              <ArrowUp />
+            </IconButton>
+
+          </li>
+          <li>
+            <IconButton>
+              <ArrowDown />
+            </IconButton>
+          </li>
+        </ul>
+      </div>
     </Paper>
   );
 };
